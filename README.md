9f6f9f7479c17728cc329a598c9e7aae7979c777
João Almeida <joaofilipe90@gmail.com>
2025-05-09 15:44:10 +0200

    separate bundle

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # yafhiruploader
 
 Yet Another FHIR IG Uploader
+Clearly inspired by the top notch ones:
+1. [https://github.com/jkiddo/ember](https://github.com/jkiddo/ember)
+2. [https://github.com/brianpos/UploadFIG](https://github.com/brianpos/UploadFIG)
 
 ### How to
 
@@ -8,5 +11,9 @@ Yet Another FHIR IG Uploader
 docker run --rm \
     -e TGZ_FILE_URL=https://example.com/package.tgz \
     -e SERVER_URL=https://fhirserver.com/fhir \
-    fhir-uploader
+    jfcal/yafhiriguploader
 ```
+
+or 
+```python upload_fhir.py https://example.com/file.tgz https://server.com --separate_bundles False```
+
--- upload_fhir.py
+++ upload_fhir.py
@@ -1,6 +1,6 @@
+import argparse
 import json
 import os
-import sys
 import tarfile
 import tempfile
 
@@ -35,7 +35,7 @@ def collect_fhir_files(example_folder_path):
     return fhir_files
 
 
-def upload_fhir_files(fhir_files, server_url):
+def upload_fhir_files(fhir_files, server_url, separate_bundles=True):
     failed_files = []
 
     for file_path in fhir_files:
@@ -48,33 +48,74 @@ def upload_fhir_files(fhir_files, server_url):
                 print(f"Skipping invalid or incomplete JSON: {file_path}")
                 continue
 
-            response = requests.put(f"{server_url}/{resourcetype}/{_id}", json=resource)
-            if response.status_code in (200, 201):
-                print(
-                    f"Uploaded {os.path.basename(file_path)}: Success ({response.status_code})"
-                )
+            if resourcetype == "Bundle" and separate_bundles:
+                for inner_resource in resource["entry"]:
+                    inner_resource_id = inner_resource["resource"]["id"]
+                    inner_resource_type = inner_resource["resource"]["resourceType"]
+                    response = requests.put(
+                        f"{server_url}/{inner_resource_type}/{inner_resource_id}",
+                        json=inner_resource["resource"],
+                    )
+                    if response.status_code in (200, 201):
+                        print(
+                            f"Uploaded {os.path.basename(file_path)}: INNER RESOURCE: {inner_resource_type}-{inner_resource_id}: Success ({response.status_code})"
+                        )
+                    else:
+                        print(
+                            f"Failed {os.path.basename(file_path)}: INNER RESOURCE: {inner_resource_type}-{inner_resource_id}: ({response.status_code}) - {response.text}"
+                        )
+                        failed_files.append(
+                            {
+                                "file": file_path,
+                                "status": response.status_code,
+                                "message": response.text,
+                            }
+                        )
             else:
-                print(
-                    f"Failed {os.path.basename(file_path)}: ({response.status_code}) - {response.text}"
+                response = requests.put(
+                    f"{server_url}/{resourcetype}/{_id}", json=resource
                 )
-                failed_files.append(
-                    {
-                        "file": file_path,
-                        "status": response.status_code,
-                        "message": response.text,
-                    }
-                )
+
+                if response.status_code in (200, 201):
+                    print(
+                        f"Uploaded {os.path.basename(file_path)}: Success ({response.status_code})"
+                    )
+                else:
+                    print(
+                        f"Failed {os.path.basename(file_path)}: ({response.status_code}) - {response.text}"
+                    )
+                    failed_files.append(
+                        {
+                            "file": file_path,
+                            "status": response.status_code,
+                            "message": response.text,
+                        }
+                    )
 
     return failed_files
 
 
 def main():
-    if len(sys.argv) != 3:
-        print("Usage: python upload_fhir.py <tgz_file_url> <server_url>")
-        sys.exit(1)
+    parser = argparse.ArgumentParser(
+        description="Upload FHIR resources from a tgz file."
+    )
+    parser.add_argument("tgz_file_url", help="URL of the .tgz package")
+    parser.add_argument("server_url", help="FHIR server base URL")
+    parser.add_argument(
+        "--separate_bundles",
+        default=True,
+        help="Split bundles into individual resources to post",
+    )
 
-    tgz_file_url = sys.argv[1]
-    server_url = sys.argv[2]
+    args = parser.parse_args()
+
+    tgz_file_url = args.tgz_file_url
+    server_url = args.server_url
+    separate_bundles = args.separate_bundles
+
+    print(f"TGZ URL: {tgz_file_url}")
+    print(f"Server URL: {server_url}")
+    print(f"Optional Param: {separate_bundles}")
 
     with tempfile.TemporaryDirectory() as temp_dir:
         tgz_path = os.path.join(temp_dir, "package.tgz")
@@ -87,11 +128,11 @@ def main():
         max_attempts = 4
         attempt = 1
         failed_files = [{"file": f} for f in fhir_files]
-        print(bool(failed_files))
+        # print(bool(failed_files))
         while attempt <= max_attempts and failed_files:
             print(f"\nAttempt {attempt} - Uploading {len(failed_files)} files...")
             failed_paths = [f["file"] for f in failed_files]
-            failed_files = upload_fhir_files(failed_paths, server_url)
+            failed_files = upload_fhir_files(failed_paths, server_url, separate_bundles)
             attempt += 1
 
         if failed_files:
@@ -117,7 +158,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-    # ▓▒░ ~   13:12  docker run jkiddo/ember
-    # --location=https://hl7-eu.github.io/unicom-ig/package.tgz
-    # --serverBase=https://sandbox.hl7europe.eu/unicom/fhir --docsAndLists=true
